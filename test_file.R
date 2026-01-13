79a512a668e0d4522388c25b9b72dd04ff4ff962
ljnewediuk <j.newedi@gmail.com>
2026-01-13 09:09:17 -0700

    Create test_file.R

---
+++ test_file.R
@@ -0,0 +1,8 @@
+
+## This is just a test file to demonstrate some coding
+
+# Make a vector of numbers
+number_vec <- c(1, 2 , 3, 4, 5)
+
+# Make a vector of letters
+letters_vec <- c('A', 'B', 'C', 'D', 'E')
