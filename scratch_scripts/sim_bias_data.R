fbbd74a682446168d06044af8c03d5276e04285c
ljnewediuk <40477861+ljnewediuk@users.noreply.github.com>
2026-03-23 10:56:43 -0600

    change simulations

--- scratch_scripts/sim_bias_data.R
+++ scratch_scripts/sim_bias_data.R
@@ -1,132 +1,148 @@
 
 # 1- Simulate multicollinearity ====
 
-# Students model number of dead fish/microbial growth across lakes.
-# They think oxygen matters, but oxygen is strongly correlated with algae biomass.
-# Both affect microbial growth, which affects dead fish.
+# Students model the effect of total winter snowfall and average spring 
+# temperature on crop yield (wheat, in bushels/acre) in 60 farms from 2021-2025.
 
-# They include both → multicollinearity (and oxygen has no apparent effect.
+# Snowfall and temperature both significantly increase yield. However, years
+# with little snowfall tend to have higher average temperature in the spring.
+
+# If they include both, there is multicollinearity and neither snowfall nor
+# temperature has an effect.
 
 set.seed(1)
-n <- 60   # lakes
+n <- 60   # Number of farms
 
 # Generate variables
-oxygen <- rnorm(n, 8, 1)
-algae <- oxygen + rnorm(n, 0, 0.3)   # strongly correlated
-microbe <- 2*oxygen + 2*algae + rnorm(n,0,2)
-dead_fish <- 5 + 3*microbe + rnorm(n,0,5)
+snow_cm <- rnorm(n, 50, 10)
+temp_C <- 50 - snow_cm + rnorm(n, 0, 0.5)   # strongly correlated
+yield_ba <- 3*snow_cm + 3*-temp_C + rnorm(n,0,10)
 
 # Data frame
 dat_multi <- data.frame(
-  oxygen,
-  algae,
-  microbe,
-  dead_fish
+  snow_cm,
+  temp_C,
+  yield_ba
 )
 
-cor(dat_multi)
+# Exclude negative yields
+dat_multi <- dat_multi[dat_multi$yield > 0 ,]
+
+# Round variables
+dat_multi_r <- as.data.frame(apply(dat_multi, 2, function(x) round(x, 1)))
+
+cor(dat_multi_r)
 
 # Correct model (total effect of oxygen)
-m1 <- lm(microbe ~ oxygen, data = dat_multi)
+m1 <- lm(yield_ba ~ snow_cm, data = dat_multi_r)
 summary(m1)
 
 # Correct model (total effect of algae)
-m2 <- lm(microbe ~ algae, data = dat_multi)
+m2 <- lm(yield_ba ~ temp_C, data = dat_multi_r)
 summary(m2)
 
 # Biased model
-m3 <- lm(microbe ~ oxygen + algae, data = dat_multi)
+m3 <- lm(yield_ba ~ snow_cm + temp_C, data = dat_multi_r)
 summary(m3)
+
+# Save the data
+write.csv(dat_multi_r, 'crop_yields.csv', row.names = F)
 
 # 2 - Simulate post-treatment bias ====
 
-# They culture the microbe and test a chemical treatment.
+# Students now model the effect of fertilizer on plant size and fruit number in
+# tomato plants. 
 
-# Process:
-# Treatment → reduces growth
-# Growth → increases toxin
-# Toxin → kills fish cells
-
-# We want:
-#   Does treatment improve survival?
-#   Students mistakenly control for toxin.
+# Fertilizer increases plant size and fruit number, and bigger plants produce 
+# more fruit. However, if they include both fertilizer and plant size (control
+# for plant size), fertilizer will appear to have no effect on fruit number.
 
 set.seed(2)
-n <- 500
+n <- 50
 
 # Generate variables
-treatment <- rbinom(n, 1, 0.5)
-growth <- 2 - 1.2*treatment + rnorm(n,0,1)
-toxin <- 1.5*growth + rnorm(n,0,1)
-linpred <- 1.0*treatment - 1.2*toxin
-prob_surv <- plogis(linpred)
-survival <- rbinom(n, 1, prob_surv)
+fertilizer_mg <- rnorm(n, 50, 10)
+plant_height_cm <- .25*fertilizer_mg + rnorm(n,0,1)
+# Generate linear model
+loglambda <- 0.02*fertilizer_mg + 0.005*plant_height_cm
+lambda <- exp(loglambda)
+# Generate poisson variable
+fruit_count <- rpois(n, lambda = lambda)
 
 # Data frame
 dat_post <- data.frame(
-  treatment,
-  growth,
-  toxin,
-  survival
+  fertilizer_mg,
+  plant_height_cm,
+  fruit_count
 )
 
-# Correct model (total effect of treatment)
-m1 <- glm(survival ~ treatment,
-          data = dat_post,
-          family = binomial)
+# Round first two variables
+dat_post_r <- as.data.frame(apply(dat_post[,1:2], 2, function(x) round(x, 1)))
+dat_post_r$fruit_count <- dat_post$fruit_count
+
+# Correct model (total effect of fertilizer)
+m1 <- glm(fruit_count ~ fertilizer_mg,
+          data = dat_post_r,
+          family = poisson)
 
 summary(m1)
 
 # Biased model (treatment has no effect)
-m2 <- glm(survival ~ treatment + toxin,
-          data = dat_post,
-          family = binomial)
+m2 <- glm(fruit_count ~ fertilizer_mg + plant_height_cm,
+          data = dat_post_r,
+          family = poisson)
 
 summary(m2)
 
+# Save the data
+write.csv(dat_post_r, 'tomatoes.csv', row.names = F)
+
 # 3 - Simulate collider bias ====
 
-# Different microbial cultures vary in toxicity
+# Students want to know the relationship between wine price and quality (are 
+# higher-priced bottles better quality?)
 
-# Toxicity depends on:
-  # Temperature
-  # Nutrient concentration
+# Bottle price affects critical acclaim score. Quality also affects critical
+# acclaim score. The students condition on acclaim score to test the association
+# between price and quality, which induces an association, even though their is
+# none.
 
 # Students measure toxin production and want to know what affects it.
 # But they control for growth rate. Growth rate is affected by both temperature 
 # and nutrients → collider.
 
 # DAG
-# mutation → growth ← nutrients
-# growth → toxin
-# mutation → toxin
+# price → acclaim ← quality
 
 set.seed(3)
-n <- 500
+n <- 80
 
 # Generate variables
-temperature <- rnorm(n,0,1)
-nutrient <- rnorm(n,0,1)
-growth <- 1.2*temperature + 1.2*nutrient + rnorm(n,0,1)
-toxin <- 1.5*temperature + 0.0*nutrient + 1.0*growth + rnorm(n,0,1)
+quality <- rnorm(n,8,1)
+price <- rnorm(n,70,15)
+acclaim <- -75 + 8*quality + 0.8*price + rnorm(n,0,3)
 
+# Data frame with rounding
 dat_coll <- data.frame(
-  temperature,
-  nutrient,
-  growth,
-  toxin
+  quality_score = round(quality, 1),
+  price_CAD = round(price, 2),
+  acclaim = round(acclaim, 0)
 )
 
-# Correct model
-m1 <- lm(toxin ~ temperature + nutrient, data = dat_coll)
+# Save the data
+write.csv(dat_coll, 'wine_quality.csv', row.names = F)
+
+# Correct model (no association)
+m1 <- lm(quality_score ~ price_CAD, data = dat_coll)
 
 summary(m1)
 
-# Biased model (conditioned on the collider)
-m2 <- lm(toxin ~ temperature + nutrient + growth, data = dat_coll)
+# Correct model (acclaim increases with quality)
+m2 <- lm(acclaim ~ quality_score, data = dat_coll)
 
 summary(m2)
 
+# Biased model (conditioned on the collider)
+m3 <- lm(quality_score ~ price_CAD + acclaim, data = dat_coll)
 
-
-
+summary(m3)
